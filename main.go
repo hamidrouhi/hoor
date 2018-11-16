edd3acc374ddcccefe41272adfd106c14d2cc019
Reza Seyf <rseyf2017@gmail.com>
2018-11-16 20:51:53 +0330

    bugfix

--- main.go
+++ main.go
@@ -2,7 +2,7 @@
 // Learn more by running `hoor` command in your command line.
 package main
 
-import "github.com/alireza-ahmadi/hoor/commands"
+import "github.com/rseyf/hoor/commands"
 
 func main() {
 	commands.Setup()
